13a87e2ddb83ce54aeb924bbfebf1cd57325ea79
jones <35585020+fleeced@users.noreply.github.com>
2023-10-16 14:22:13 -0600

    added text on top of banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Banner: React.FC = () => {
-  return <div className="banner"></div>;
+  return (
+    <div className="banner relative flex items-center justify-center">
+      <div className="absolute top-40 text-white text-3xl font-bold">
+        Better Refs.
+      </div>
+      <div className="absolute top-48 text-white text-3xl font-bold">
+        Better Hockey.
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
