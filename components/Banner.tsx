ecc43118ca2b05035eae1a8ce315dbba786c2d8f
jones <35585020+fleeced@users.noreply.github.com>
2023-10-16 16:02:51 -0600

    logo fades in and slides up after text

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,15 +5,18 @@ import React, { useState, useEffect } from "react";
 const Banner: React.FC = () => {
   const [showFirstText, setShowFirstText] = useState(false);
   const [showSecondText, setShowSecondText] = useState(false);
+  const [showLogo, setShowLogo] = useState(false);
 
   useEffect(() => {
-    const timer1 = setTimeout(() => setShowFirstText(true), 500); // Delay of 500ms
-    const timer2 = setTimeout(() => setShowSecondText(true), 2000); // Delay of 1500ms
+    const timer1 = setTimeout(() => setShowFirstText(true), 500);
+    const timer2 = setTimeout(() => setShowSecondText(true), 2000);
+    const timer3 = setTimeout(() => setShowLogo(true), 3500);
 
     // Cleanup timers on component unmount
     return () => {
       clearTimeout(timer1);
       clearTimeout(timer2);
+      clearTimeout(timer3);
     };
   }, []);
 
@@ -29,6 +32,15 @@ const Banner: React.FC = () => {
           <div className="text-white text-3xl font-bold fade-in ">
             Better Hockey.
           </div>
+        )}
+        {showLogo && (
+          <img
+            src="/logo.png"
+            alt="Logo"
+            className="mt-20 slide-up"
+            width="150"
+            height="150"
+          />
         )}
       </div>
     </div>
