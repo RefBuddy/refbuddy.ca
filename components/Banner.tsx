e5a3ad614e99f8a96a3f5b7c474dcd8d0574c788
jones <35585020+fleeced@users.noreply.github.com>
2023-10-16 15:13:57 -0600

    it looks kinda good on web and mobile

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 
 const Banner: React.FC = () => {
+  const [showFirstText, setShowFirstText] = useState(false);
+  const [showSecondText, setShowSecondText] = useState(false);
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => setShowFirstText(true), 500); // Delay of 500ms
+    const timer2 = setTimeout(() => setShowSecondText(true), 2000); // Delay of 1500ms
+
+    // Cleanup timers on component unmount
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, []);
+
   return (
     <div className="banner relative flex items-center justify-center">
-      <div className="absolute top-40 text-white text-3xl font-bold">
-        Better Refs.
-      </div>
-      <div className="absolute top-48 text-white text-3xl font-bold">
-        Better Hockey.
-      </div>
+      {showFirstText && (
+        <div className="absolute top-36 text-white text-3xl font-bold fade-in">
+          Better Refs.
+        </div>
+      )}
+      {showSecondText && (
+        <div className="absolute top-44 text-white text-3xl font-bold fade-in">
+          Better Hockey.
+        </div>
+      )}
     </div>
   );
 };
