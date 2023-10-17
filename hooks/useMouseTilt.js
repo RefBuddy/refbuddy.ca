f40fc99ca53be37b8aee2101bab812cbaa3f4ecf
jones <35585020+fleeced@users.noreply.github.com>
2023-10-17 15:30:19 -0600

    more responsive tilt

--- hooks/useMouseTilt.js
+++ hooks/useMouseTilt.js
@@ -8,32 +8,17 @@ const useMouseTilt = () => {
       if (!ref.current) return;
       const img = ref.current.querySelector('img');
       const rect = ref.current.getBoundingClientRect();
-      const x = ((e.clientX - rect.left) / rect.width) * 100;
-      const y = ((e.clientY - rect.top) / rect.height) * 100;
       
-      let rotateX = 0;
-      let rotateY = 0;
+      // Calculate the distance of the cursor from the center of the logo in percentage
+      const offsetX = (((e.clientX - rect.left) / rect.width) - 0.5) * 100;
+      const offsetY = (((e.clientY - rect.top) / rect.height) - 0.5) * 100;
       
-      if (x > 50 && y < 50) {
-        // Top right
-        rotateX = 5;
-        rotateY = 5;
-      } else if (x > 50 && y > 50) {
-        // Bottom right
-        rotateX = -5;
-        rotateY = 5;
-      } else if (x < 50 && y > 50) {
-        // Bottom left
-        rotateX = -5;
-        rotateY = -5;
-      } else {
-        // Top left
-        rotateX = 5;
-        rotateY = -5;
-      }
+      // Convert the percentage to rotation angles (max ±5 degrees)
+      const rotateX = -offsetY * 0.2; // Negative because moving the mouse up should tilt the logo forward
+      const rotateY = offsetX * 0.2;
       
       img.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    };
+  };  
     
     const handleMouseLeave = () => {
       if (!ref.current) return;
