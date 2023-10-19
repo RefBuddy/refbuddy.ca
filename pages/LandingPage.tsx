7c25399231772fdf01edf43d54ff61f877ea31af
jones <35585020+fleeced@users.noreply.github.com>
2023-10-19 16:42:12 -0600

    smoother scrolling

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -10,18 +10,45 @@ const LandingPage: React.FC = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [scrollY, setScrollY] = useState(0);
 
+  let rafId: number | null = null;
+
+  const lerp = (a: number, b: number, n: number) => (1 - n) * a + n * b;
+
+  const handleScroll = () => {
+    if (window.scrollY > 171 && !rafId) {
+      rafId = requestAnimationFrame(smoothScrollToTarget);
+    } else {
+      setScrollY(window.scrollY);
+    }
+  };
+
+  const smoothScrollToTarget = () => {
+    const currentScroll = window.scrollY;
+    const targetScroll = 171;
+    const newScroll = lerp(currentScroll, targetScroll, 0.1);
+
+    if (Math.abs(newScroll - currentScroll) < 0.5) {
+      window.scrollTo(0, targetScroll);
+      cancelAnimationFrame(rafId!);
+      rafId = null;
+      return;
+    }
+
+    window.scrollTo(0, newScroll);
+    rafId = requestAnimationFrame(smoothScrollToTarget);
+  };
+
   useEffect(() => {
     setIsLoaded(true);
   }, []);
 
   useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      if (rafId) {
+        cancelAnimationFrame(rafId);
+      }
     };
   }, []);
 
